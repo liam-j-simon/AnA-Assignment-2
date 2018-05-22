ccdc6a16fce0eb354adbf11e3652e38b10529078
rmit-s3645417-seanmartin <31027655+rmit-s3645417-seanmartin@users.noreply.github.com>
2018-05-22 19:37:13 +1000

    Added loader class (does nothing yet)

---
+++ javaSrc/Loader.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class Loader {
+	
+	String gameFinename;
+	Collection<Character> characters;
+	HashMap<String, ArrayList<String>> attributes;
+	
+	public Loader(String gameFilename) {
+		this.gameFinename = gameFilename;
+	}
+	
+	public void loadAttributes() {
+	
+	}
+	
+	public void loadCharacters() {
+	
+	}
+	
+	public Collection<Character> getCharacters() {
+		return characters;
+	}
+	
+	public HashMap<String, ArrayList<String>> getAttributes() {
+		return attributes;
+	}
+}
