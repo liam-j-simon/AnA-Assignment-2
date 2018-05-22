db3c4b930ebee342c1629f58a9ed2f2b2bcc1f9e
rmit-s3645417-seanmartin <31027655+rmit-s3645417-seanmartin@users.noreply.github.com>
2018-05-22 22:06:53 +1000

    changes

--- javaSrc/Loader.java
+++ javaSrc/Loader.java
@@ -4,34 +4,94 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 public class Loader {
 	
 	private String gameFilename;
-	private Collection<Character> characters;
-	private HashMap<String, ArrayList<String>> attributes;
+	private List<Character> characters;
+	private HashMap<String, List<String>> allAttributes;
+	private HashMap<String, String> playerAttributes;
 	
 	public Loader(String gameFilename) {
 		this.gameFilename = gameFilename;
+		
+		characters = new ArrayList<>();
+		allAttributes = new HashMap<>();
+		playerAttributes = new HashMap<>();
 	}
 	
 	public void loadAttributes() {
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(gameFilename));
-		} catch (IOException e) {
-		
+			String line;
+			
+			loadAttributes(in);
+			loadCharacters(in);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public void loadCharacters() {
-
+	public void loadAttributes(BufferedReader in) throws Exception {
+		
+		String line;
+		String [] tokens;
+		List<String> attributes = new ArrayList<>();
+		
+		/* While lines are left in the file */
+		while ((line = in.readLine()).length() > 0) {
+				
+			/* Splits a string by white space*/
+			tokens = line.trim().split("\\s+");
+			
+			/* Creates the ArrayList of allAttributes */
+			for (int i = 1; i < tokens.length; i++)
+				attributes.add(tokens[i]);
+			
+			/* Sets key and value in HashMap */
+			this.allAttributes.put(tokens[0], attributes);
+			
+			attributes.clear();
+		}
 	}
 	
-	public Collection<Character> getCharList() {
+	public void loadCharacters(BufferedReader in) throws Exception {
+		
+		String line;
+		String [] tokens;
+		String charName = null;
+		
+		/* While lines are left in the file */
+		while ((line = in.readLine()) != null) {
+			
+			/* If line is not blank */
+			if (line.trim().length() > 0) {
+				
+				/* Splits a string by white space*/
+				tokens = line.trim().split("\\s+");
+				
+				/* If single token, set name */
+				if (tokens.length == 1) {
+					charName = tokens[0];
+				/* Store the attribute in HashMap */
+				} else {
+					playerAttributes.put(tokens[0], tokens[1]);
+				}
+			/* Create character at blank line */
+			} else {
+				characters.add(new Character(charName, playerAttributes));
+				playerAttributes.clear();
+			}
+		}
+	}
+	
+	public Collection<Character> getCharacters() {
 		return characters;
 	}
 	
-	public HashMap<String, ArrayList<String>> getPlayerAttributes() {
-		return attributes;
+	public HashMap<String, List<String>> getAllAttributes() {
+		return allAttributes;
 	}
 }
