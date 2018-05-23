becd94df5a8656c8628e0c102dba02678d04ed66
rmit-s3645417-seanmartin <31027655+rmit-s3645417-seanmartin@users.noreply.github.com>
2018-05-23 15:48:30 +1000

    binary fully commented

--- javaSrc/BinaryGuessPlayer.java
+++ javaSrc/BinaryGuessPlayer.java
@@ -26,67 +26,68 @@ public class BinaryGuessPlayer extends GuessPlayer implements Player
         super(gameFilename, chosenName);
     } // end of BinaryGuessPlayer()
 
-
+    /* Binary search guessing: Finds the attribute that is closest to removing
+     * half of the remaining characters */
     public Guess guess() {
         /* Number of attribute matches for remaining characters */
-        int match = 0, notMatch = 0;
-        /* The difference between 'half the number of characters remaining'
-         * and the closest number of matches/not matches */
-        int minDifference = Math.abs((int) Math.ceil(characters.size() / 2.0));
-        
-        System.out.println("Char: " + characters.size());
-        System.out.println(minDifference + "\n\n");
-        
+        int match = 0, nonMatch = 0;
+        /* The minimum difference between 'half the number of characters remaining'
+         * and the closest 'match/non-match'. Initially set as half the number of
+         * characters remaining (largest possible difference) */
+        int minDifference = characters.size() / 2;
+        /* Stores the guess with the minDifference */
         String attributeGuess = null;
         String valueGuess = null;
         
+        /* If there is more than 1 character remaining */
         if(characters.size() > 1) {
             /* For each attribute */
             earlyTerminate:
             for (String attribute : pAttributes.keySet()) {
-                /* For each attribute value */
+                /* For each attribute value remaining */
                 for (String value : pAttributes.get(attribute)) {
-                    /* For each character */
+                    /* For each character remaining */
                     for (Character character : characters.values()) {
                         /* If the characters attribute matches the value */
                         if (character.getAttributes().get(attribute).equals(value)) {
                             match++;
                         } else {
-                            notMatch++;
+                            nonMatch++;
                         }
                     }
                     
-                    /* Compare */
+                    /* Compares the matches and non-matches and chooses
+                     * the lowest difference */
                     int difference =
-                            Math.abs((characters.size() / 2) - match) >
-                            Math.abs((characters.size() / 2) - notMatch) ?
+                            Math.abs((characters.size() / 2) - match) <
+                            Math.abs((characters.size() / 2) - nonMatch) ?
                             Math.abs((characters.size() / 2) - match) :
-                            Math.abs((characters.size() / 2) - notMatch);
+                            Math.abs((characters.size() / 2) - nonMatch);
                     
+                    /* Compares the current difference with the minimum
+                     * difference so far and sets the relevant attribute value */
                     if (difference < minDifference) {
                         attributeGuess = attribute;
                         valueGuess = value;
                         minDifference = difference;
                     }
-                    System.out.println("Difference: " + difference);
-                    System.out.println("MinDifference: " + minDifference + "\n");
                     
-                    /* Stop searching if minimum difference is found.*/
+                    /* Reset counters between attribute values */
+                    match = 0;
+                    nonMatch = 0;
+    
+                    /* Early termination if minimum possible
+                     * difference is found.*/
                     if (difference == 0)
                         break earlyTerminate;
-                    
-                    match = 0;
-                    notMatch = 0;
                 }
             }
-    
-            if (attributeGuess == null || valueGuess == null) {
-                System.out.println("test");
-            }
             
+            /* Remove the value guessed and make guess */
             pAttributes.get(attributeGuess).remove(valueGuess);
             return new Guess(Guess.GuessType.Attribute, attributeGuess, valueGuess);
-
+        
+        /* If there is 1 character remaining, guess the character */
         } else {
             return new Guess(Guess.GuessType.Person,"", characters.get(
                     new ArrayList<>(characters.keySet()).get(0)).getName());
